441e175c7141597df22dfa9c2d07287c143e42a0
weirdev <weirdev@outlook.com>
2023-05-07 09:39:58 -0400

    BList store as bytes

--- src/bint/blist.rs
+++ src/bint/blist.rs
@@ -1,28 +1,36 @@
 use serde_json::Value;
+use std::marker::PhantomData;
+use std::mem::transmute;
 
 use super::{buint32::BUInt32, FromJsonValue, ToRust};
 
 pub struct BList<T> {
-    length: BUInt32,
-    data: Box<[T]>,
+    element_type: PhantomData<*const T>,
+    data: Box<[u8]>,
 }
 
 impl<T: FromJsonValue<T>> FromJsonValue<BList<T>> for BList<T> {
     fn from_json_value(value: &serde_json::Value) -> BList<T> {
         if let Value::Array(values) = value {
+            let typed_box: Box<[T]> = values.iter().map(|v| T::from_json_value(v)).collect();
             return BList {
-                length: BUInt32 {
-                    data: (values.len() as u32).to_le_bytes(),
-                },
-                data: values.iter().map(|v| T::from_json_value(v)).collect(),
+                element_type: PhantomData,
+                data: unsafe { transmute(typed_box) },
             };
         }
         panic!("Attempting to construct BList from non-array")
     }
 }
 
+impl<'a, T> BList<T> {
+    fn get_elements_as_slice(&'a self) -> &'a [T] {
+        let typed_box: &Box<[T]> = unsafe { transmute(&self.data) };
+        typed_box.as_ref()
+    }
+}
+
 impl<'a, T> ToRust<'a, &'a [T]> for BList<T> {
     fn to_rust(&'a self) -> &'a [T] {
-        self.data.as_ref()
+        self.get_elements_as_slice()
     }
 }
